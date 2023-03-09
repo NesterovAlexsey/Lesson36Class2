5a802c7a8e9543fb9dcc77afeb5563011077016d
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-09 16:05:33 +0100

    Create class Soda and add Constructor

---
+++ src/Soda.java
@@ -0,0 +1,16 @@
+public class Soda {
+
+  private String addition;
+
+  public Soda(String addition) { //this is constructor!
+    this.addition = addition;
+  }
+
+  public void printInfo() {
+    System.out.println(addition);
+  }
+
+//  public String GetMyDrinkString() {
+//  return
+//  }
+}
